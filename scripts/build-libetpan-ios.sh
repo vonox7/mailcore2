667a760bfb295605f4fcef0ece39a71e523a692b
Valentin Slawicek <valentin.slawi@gmail.com>
2021-03-29 18:22:08 +0200

    Update build-libetpan-ios.sh

--- scripts/build-libetpan-ios.sh
+++ scripts/build-libetpan-ios.sh
@@ -7,7 +7,7 @@ popd > /dev/null
 . "$scriptpath/include.sh/build-dep.sh"
 
 url="https://github.com/dinhviethoa/libetpan.git"
-rev=5164ba2ebd3c7cbc7a9230aad32bdf8e24e207de
+rev=a6e6fb1b9d298c690db2eafb48a853e5b26c68d5
 name="libetpan-ios"
 xcode_target="libetpan ios"
 xcode_project="libetpan.xcodeproj"
